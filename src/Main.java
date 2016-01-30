44bf4167cf9a1cdd4d39703360e20de6aefb28ff
grizzly <dimaodnoiko@gmail.com>
2016-01-31 01:59:29 +0200

    v1.3

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by grizzly on 1/30/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("endless");
+        System.out.println("endless");
+        int a=1;
+    }
+    int as(int a)
+    {
+        a=a^2;
+    }
+}
